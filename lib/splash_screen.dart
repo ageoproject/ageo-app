eea8c2655021dd0e8505eef5620fda36c10e7f68
Manish Singh <manish@blackcurrantlabs.com>
2022-10-19 15:21:00 +0530

    fix splash screen animation for mobile and tablet

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -9,23 +9,22 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
 
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(milliseconds: 500),
-    vsync: this,
-  );
+  late final AnimationController _controller ;
 
   late final Animation<Offset> _offsetAnimation;
 
-  @override
-  void initState() {
-    super.initState();
-
-  }
 
   Future<void> startAnimation()async{
-    Future.delayed(const Duration(milliseconds: 500),(){
+    _controller = AnimationController(
+      duration:const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0, 5.5),
+      end: Offset.zero,
+    ).animate(_controller);
+    Future.delayed(const Duration(milliseconds: 300),(){
       _controller.forward();
-      print("Animation started");
     });
   }
 
@@ -37,43 +36,25 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
 
   @override
   Widget build(BuildContext context) {
+    startAnimation();
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context,constraints){
-          print("offset(${constraints.biggest},${constraints.maxHeight})");
-          _offsetAnimation = Tween<Offset>(
-            begin: const Offset(0, 10),
-            end: Offset.zero,
-          ).animate(_controller);
-          startAnimation();
-          return Stack(
-            alignment: Alignment.center,
-            children: [
-              Opacity(
-                opacity: 0.12,
-                child: Image.asset("assets/images/splash_screen/splash_screen_bg.png",width: MediaQuery.of(context).size.width,height: MediaQuery.of(context).size.height,fit: BoxFit.fill,),
-              ),
-              Positioned(
-                top: MediaQuery.of(context).size.width/2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 125),
-                      child: Text("Welcome",style: TextStyle(fontSize: 29),),
-                    ),
-
-                    SlideTransition(
-                      position: _offsetAnimation,
-                      child: Image.asset("assets/images/splash_screen/ageo_logo.png",width: 265,),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          );
-        },
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Opacity(
+            opacity: 0.12,
+            child: Image.asset("assets/images/splash_screen/splash_screen_bg.png",width: MediaQuery.of(context).size.width,height: MediaQuery.of(context).size.height,fit: BoxFit.fill,),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.width/2,
+            child: SlideTransition(
+              position: _offsetAnimation,
+              child: Image.asset("assets/images/splash_screen/ageo_logo.png",width: 265,),
+            ),
+          )
+        ],
       ),
+
     );
   }
 }
