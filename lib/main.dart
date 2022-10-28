7ed7b61726ea4d569af830a35f417a1a0fe39d6d
Manish Singh <manish@blackcurrantlabs.com>
2022-10-28 15:31:31 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
